abb002372afac2a5119d6451169f614a199bd01b
uma <umasit77@gmail.com>
2023-10-05 11:15:30 +0530

    QA Commit

--- src/test/java/org/simple/SampleProgram.java
+++ src/test/java/org/simple/SampleProgram.java
@@ -8,6 +8,11 @@ public class SampleProgram {
 		
 		s.contains("m");
 		
+		System.out.println("woking on Calls");
+		System.out.println("done by B");
+		System.out.println("Running Successfully");
+
+		
 
 	}
 
